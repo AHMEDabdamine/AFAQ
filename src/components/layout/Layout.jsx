705646cb9d6d123b3ce09f41df10e5620dd77fc0
AHMEDabdamine <ahmedabdesselam55@gmail.com>
2026-07-03 15:02:17 +0100

    feat: implement smooth scrolling with GSAP and Lenis in layout component

--- src/components/layout/Layout.jsx
+++ src/components/layout/Layout.jsx
@@ -1,15 +1,18 @@
 import { Outlet } from 'react-router-dom'
 import Navbar from './Navbar'
 import Footer from './Footer'
+import SmoothScroll from '../shared/SmoothScroll'
 
 export default function Layout() {
   return (
-    <div className="min-h-screen flex flex-col" style={{ background: 'var(--color-bg)', color: 'var(--color-text)' }}>
-      <Navbar />
-      <main className="flex-1">
-        <Outlet />
-      </main>
-      <Footer />
-    </div>
+    <SmoothScroll>
+      <div className="min-h-screen flex flex-col" style={{ background: 'var(--color-bg)', color: 'var(--color-text)' }}>
+        <Navbar />
+        <main className="flex-1">
+          <Outlet />
+        </main>
+        <Footer />
+      </div>
+    </SmoothScroll>
   )
 }
---
+++ src/components/shared/SmoothScroll.jsx
@@ -0,0 +1,27 @@
+import { useEffect } from 'react'
+import { ReactLenis } from 'lenis/react'
+import gsap from 'gsap'
+import { ScrollTrigger } from 'gsap/ScrollTrigger'
+
+gsap.registerPlugin(ScrollTrigger)
+
+export default function SmoothScroll({ children }) {
+  useEffect(() => {
+    const update = () => ScrollTrigger.refresh()
+    update()
+    return () => {}
+  }, [])
+
+  return (
+    <ReactLenis
+      root
+      options={{
+        duration: 1.2,
+        easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t)),
+        smoothWheel: true,
+      }}
+    >
+      {children}
+    </ReactLenis>
+  )
+}
